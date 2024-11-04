a63b9feec85617963959204951e4779e8ec8add8
Wiktor Korol <wiktor.korol@iteo.com>
2024-11-04 10:29:53 +0100

    fixes

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,8 +4,7 @@
 # Set ownership of the var directory
 chown -R www-data:www-data /var/www/html/var
 
-php bin/console lexik:jwt:generate-keypair --overwrite
-php bin/console cache:clear
+#php bin/console lexik:jwt:generate-keypair --overwrite
 php bin/console doctrine:migrations:migrate --no-interaction
 
 # Run any additional startup commands here, then start Apache

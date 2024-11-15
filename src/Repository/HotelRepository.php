e699f5a580298caad128edbe23add1d69ff67815
Wiktor Korol <wiktor.korol@iteo.com>
2024-11-15 08:21:10 +0100

    Fixes

--- src/Repository/HotelRepository.php
+++ src/Repository/HotelRepository.php
@@ -76,7 +76,7 @@ class HotelRepository extends ServiceEntityRepository
             ->leftJoin('h.lump_sums', 'ls')
             ->leftJoin('h.region', 'r')
             ->leftJoin('h.new_lump_sums', 'nls')        // Join the `new_lump_sums` association in `Hotel`
-            ->addSelect('ls', 'nls')                    // Select lump sums
+            ->addSelect('ls', 'nls', 'r')                    // Select lump sums
             ->where('h.region = :id')                   // Filter by region ID
             ->setParameter('id', $id)
             ->getQuery()
